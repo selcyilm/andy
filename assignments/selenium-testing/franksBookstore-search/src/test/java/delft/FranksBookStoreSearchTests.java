7cad969b4ffb5fb3c23bbf312801527ca9fee2f6
Selcuk <selcukemnn@gmail.com>
2024-03-27 00:11:13 +0300

    done searchBook test and airway test

--- assignments/selenium-testing/franksBookstore-search/src/test/java/delft/FranksBookStoreSearchTests.java
+++ assignments/selenium-testing/franksBookstore-search/src/test/java/delft/FranksBookStoreSearchTests.java
@@ -1,8 +1,11 @@
 package delft;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -19,7 +22,67 @@ class FranksBookStoreSearchTests {
         driver = CSE1110.getDriver();
     }
 
+    //First you gotta test if there is a search box
+    @Test
+    public void searchBoxAppearTest() {
+        WebElement searchBox = driver.findElement(By.id("search_bar"));
 
+        Assertions.assertTrue(searchBox.isDisplayed());
+    }
+
+    @Test
+    public void emtySearchBoxTest() {
+        WebElement searchBox = driver.findElement(By.id("search_bar"));
+        searchBox.sendKeys(Keys.ENTER);
+
+        WebElement authorListElement = driver.findElement(By.id("author_list"));
+        String expectedAuthor1 = "Mauricio";
+        String expectedAuthor2 = "Dan Brown";
+        String expectedAuthor3 = "Tolkien";
+
+        assertThat(authorListElement.getText().contains(expectedAuthor1)).isTrue();
+        assertThat(authorListElement.getText().contains(expectedAuthor2)).isTrue();
+        assertThat(authorListElement.getText().contains(expectedAuthor3)).isTrue();
+    }
+
+    @ParameterizedTest(name = "authorName = {0}")
+    @CsvSource({"Selcuk", "Ahmet", "Test53"})
+    public void addAuthorAndSearchTest(String authorName) {
+        driver.findElement(By.id("author_name")).sendKeys(authorName);
+        driver.findElement(By.id("author_submit_button")).click();
+
+        WebElement searchBox = driver.findElement(By.id("search_bar"));
+        searchBox.sendKeys(authorName + Keys.ENTER);
+
+        String actualAuthorListElement = driver.findElement(By.id("author_list")).getText();
+        Assertions.assertTrue(actualAuthorListElement.contains(authorName));
+    }
+
+
+    @ParameterizedTest(name = "authorName = {0}, authorBook = {1}, authorId = {2}")
+    @CsvSource({"Selcuk, GDPR, 4", "Ahmet, Essays, 4", "Test53, BookForLove, 4"})
+    public void addAuthorAndBookSearchTest(String authorName, String authorBook, int id) {
+        driver.findElement(By.id("author_name")).sendKeys(authorName);
+        driver.findElement(By.id("author_submit_button")).click();
+
+        driver.findElement(By.id("book_name")).sendKeys(authorBook);
+        driver.findElement(By.id("author_id")).sendKeys(Integer.toString(id));
+        driver.findElement(By.id("book_submit_button")).click();
+
+        WebElement searchBox = driver.findElement(By.id("search_bar"));
+        searchBox.sendKeys(authorName + Keys.ENTER);
+
+        String actualAuthorListElement = driver.findElement(By.id("author_list")).getText();
+        Assertions.assertTrue(actualAuthorListElement.contains(authorName));
+
+        searchBox.clear();
+        searchBox.sendKeys(authorBook + Keys.ENTER);
+
+        String bookListElement = driver.findElement(By.id("book_list")).getText();
+        Assertions.assertTrue(bookListElement.contains(authorName));
+        Assertions.assertTrue(bookListElement.contains(authorBook));
+
+    }
     @AfterEach
     public void close() {
         driver.close();
--- assignments/selenium-testing/mauricio-airways/src/test/java/delft/MauricioAirwaysTests.java
+++ assignments/selenium-testing/mauricio-airways/src/test/java/delft/MauricioAirwaysTests.java
@@ -1,7 +1,14 @@
 package delft;
 
+import net.jqwik.web.api.Web;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+
+import java.util.List;
+import java.util.Random;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -15,9 +22,122 @@ class MauricioAirwaysTests {
         // and your final grade for this exercise will be zero.
         driver = CSE1110.createDriver();
     }
-    
-    // Write your tests here
 
+    //test if there is flights to select
+    @Test
+    public void testIfThereIsFlight() {
+        List<WebElement> flightElementsList = driver.findElements(By.className("selectFlight"));
+        //test if there is a flight
+        Assertions.assertTrue(flightElementsList.size() > 0);
+
+        //test if the flight is enabled
+        Assertions.assertTrue(flightElementsList.get(0).isEnabled());
+    }
+
+    //test the fight use select appear in selected flight
+    @ParameterizedTest(name = "flightIndex = {0}")
+    @CsvSource({"0", "1", "2", "3"})
+    public void testIfFlightAppear(int flightIndex) {
+        List<WebElement> flightElementsList = driver.findElements(By.className("selectFlight"));
+        WebElement firstFlightElement = flightElementsList.get(flightIndex);
+        firstFlightElement.click();
+        String flightFullText = firstFlightElement.getText();
+
+        WebElement flightToken = driver.findElement(By.id("selectedFlightName"));
+
+        String expectedToken = ((flightFullText.split("\n"))[1].split(" "))[1];
+        String actualToken = flightToken.getText();
+
+        Assertions.assertEquals(expectedToken, actualToken);
+    }
+
+    //test the selected fight has more than one seat available
+    @ParameterizedTest(name = "flightIndex = {0}")
+    @CsvSource({"0", "1", "2", "3"})
+    public void testIfMoreThanOneSeat(int flightIndex) {
+        List<WebElement> flightElementsList = driver.findElements(By.className("selectFlight"));
+        WebElement firstFlightElement = flightElementsList.get(flightIndex);
+        firstFlightElement.click();
+
+        int actualSeatAvailable = Integer.parseInt(driver.findElement(By.id("availableSeats"))
+                .getText());
+
+        Assertions.assertTrue(actualSeatAvailable > 0);
+    }
+
+    //book a valid seat and test your ticket is seen in reserved
+    //after you reserved, test if there is valid amount of seat remained
+    @ParameterizedTest(name = "flightIndex = {0}")
+    @CsvSource({"0", "1", "2", "3"})
+    public void testValidSeatReserved(int flightIndex) {
+        List<WebElement> flightElementsList = driver.findElements(By.className("selectFlight"));
+        WebElement firstFlightElement = flightElementsList.get(flightIndex);
+        firstFlightElement.click();
+
+        String availableSeatNumber = driver.findElement(By.id("availableSeats")).getText();
+
+        WebElement bookSeatBox = driver.findElement(By.id("selectedNumberOfSeats"));
+        bookSeatBox.sendKeys(availableSeatNumber);
+        driver.findElement(By.id("bookButton")).click();
+
+        String afterAvailableSeat = driver.findElement(By.id("availableSeats")).getText();
+
+        Assertions.assertEquals("0", afterAvailableSeat);
+
+        WebElement bookedTicketsBox = driver.findElement(By.id("ticketList"));
+        String bookedTableArr[] = bookedTicketsBox.getText().split(" ");
+
+        String expectedToken = ((firstFlightElement.getText().split("\n"))[1].split(" "))[1];
+        String actualToken = bookedTableArr[0];
+
+        Assertions.assertEquals(expectedToken, actualToken);
+
+        String actualSeatsTaken = bookedTableArr[3];
+
+        Assertions.assertEquals(availableSeatNumber, actualSeatsTaken);
+    }
+
+    //book  invalid seat (negative) and test if error message is shown
+    @ParameterizedTest(name = "flightIndex = {0}")
+    @CsvSource({"0", "1", "2", "3"})
+    public void testNegInvalidValidSeatReserved(int flightIndex) {
+        List<WebElement> flightElementsList = driver.findElements(By.className("selectFlight"));
+        WebElement firstFlightElement = flightElementsList.get(flightIndex);
+        firstFlightElement.click();
+
+
+        WebElement bookSeatBox = driver.findElement(By.id("selectedNumberOfSeats"));
+        bookSeatBox.sendKeys("" + new Random().nextInt(10) * -1);
+        driver.findElement(By.id("bookButton")).click();
+
+        String expectedErrorMessage = "Selected number of seats not possible.";
+        String actualErrorMessage = driver.findElement(By.id("alert-msg")).getText();
+        Assertions.assertEquals(expectedErrorMessage, actualErrorMessage);
+
+        driver.findElement(By.id("closeErrorButton")).click();
+    }
+
+    //book  invalid seat (above available) and test if error message is shown
+    @ParameterizedTest(name = "flightIndex = {0}")
+    @CsvSource({"0", "1", "2", "3"})
+    public void testAboveInvalidValidSeatReserved(int flightIndex) {
+        List<WebElement> flightElementsList = driver.findElements(By.className("selectFlight"));
+        WebElement firstFlightElement = flightElementsList.get(flightIndex);
+        firstFlightElement.click();
+
+
+        int actualSeatAvailable = Integer.parseInt(driver.findElement(By.id("availableSeats"))
+                .getText());
+        WebElement bookSeatBox = driver.findElement(By.id("selectedNumberOfSeats"));
+        bookSeatBox.sendKeys("" + (actualSeatAvailable + 1));
+        driver.findElement(By.id("bookButton")).click();
+
+        String expectedErrorMessage = "Selected number of seats not possible.";
+        String actualErrorMessage = driver.findElement(By.id("alert-msg")).getText();
+        Assertions.assertEquals(expectedErrorMessage, actualErrorMessage);
+
+        driver.findElement(By.id("closeErrorButton")).click();
+    }
 
     @AfterEach
     public void close() {
